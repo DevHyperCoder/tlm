c962317a825cf749c0a8fde38a685df5128b5cb4
DevHyperCoder <arduinoleo88@gmail.com>
2021-02-19 18:55:46 +0530

    Formatting using astyle

--- log.c
+++ log.c
@@ -1,10 +1,10 @@
 #include "stdio.h"
 #include "log.h"
 
-void err(char *message){
+void err(char *message) {
     fprintf(stderr, "%s",message);
 }
 
-void info(char *message){
+void info(char *message) {
     fprintf(stdout, "%s",message);
 }
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "log.h"
 
-int main(){
+int main() {
     info("TLM - Terminal Login Manager");
 }
